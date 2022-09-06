86b8216c7422d8da7fe2ef09834a378fe59c66b2
DYA-Code <entrydia7691@gmail.com>
2022-09-07 00:07:50 +0900

    navbar

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <IndexPage />
+  </div>
+</template>
+
+<script>
+
+import IndexPage from './views/IndexPage.vue';
+
+export default {
+  name: 'App',
+  components: {
+    IndexPage,
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@500&display=swap');
+
+#app {
+  /* font-family: Avenir, Helvetica, Arial, sans-serif; */
+  font-family: 'JetBrains Mono', monospace;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  color: #435566;
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ src/views/IndexPage.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    
+    <nav class="flex items-center m-14 mb-[300px]">
+      <div class="nav-brand mr-10 text-2xl font-bold">Devlog</div>
+      <a class="nav-item mr-4 text-xl" href="https://github.com/dya-code/">Github</a>
+      <a class="nav-item text-xl" href="https://discord.com/users/884954874943520788/">Discord</a>
+    </nav>
+
+    <div class="cards flex justify-center flex-wrap">
+      <div class="card m-8 w-72 h-80 bg-slate-800 rounded-2xl shadow-xl"></div>
+      <div class="card m-8 w-72 h-80 bg-slate-800 rounded-2xl shadow-xl"></div>
+      <div class="card m-8 w-72 h-80 bg-slate-800 rounded-2xl shadow-xl"></div>
+      <div class="card m-8 w-72 h-80 bg-slate-800 rounded-2xl shadow-xl"></div>
+    </div>
+
+
+  </div>
+</template>
+
+<script>
+  
+  export default {
+    name: 'IndexPage',
+  }
+</script>
+  
+<style>
+</style>
+  
